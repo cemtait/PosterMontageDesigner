fe855bf8be0fada4d66302ad26e5d8e4979c2691
Charles Tait <cemtait@gmail.com>
2026-07-05 11:51:53 +1200

    tools updated

--- tools/save.bat
+++ tools/save.bat
@@ -2,9 +2,17 @@
 set PROJECT=%~dp0..
 
 cd /d "%PROJECT%"
+
 git add .
 git status
+
 echo.
 set /p MSG=Commit message: 
+
+if "%MSG%"=="" (
+    echo Commit cancelled: no message entered.
+    exit /b 1
+)
+
 git commit -m "%MSG%"
 git push
